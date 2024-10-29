c57cd8ea5b85614e96cb38975a2206758f8dee6e
Mun Jin <miinjunn@gmail.com>
2024-10-29 22:58:11 +0700

    fix tanda baca

--- Python (basic)/fizzbuzz.py
+++ Python (basic)/fizzbuzz.py
@@ -1,12 +1,16 @@
 n = 15
 
+
 def fizzBuzz(n):
     for i in range(1, n+1):
         if i % 3 == 0 and i % 5 == 0:
-            print('Fizzbuzz')
+            print("Fizzbuzz")
         elif i % 3 == 0:
-            print('Fizz')
+            print("Fizz")
         elif i % 5 == 0:
-            print('Buzz')
+            print("Buzz")
         else:
             print(i)
+
+
+fizzBuzz(n)
